83f466eafffe8dc0a2b6e64ed2490a2af1350d24
RD-Ricardo <ricardodias169@gmail.com>
2025-01-02 23:18:52 -0300

    feat: user case create event

---
+++ src/Application/UseCases/CreateEvent/CreateEventInputModel.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+
+namespace Application.Events.CreateEvent
+{
+    public record CreateEventInputModel(
+        string Name,
+        string? Description,
+        DateTime Date,
+        string Street,
+        string Number,
+        string City,
+        string State,
+        string ZipCode,
+        string Country,
+        string? Complement
+     );
+  
+    public class CreateEventInputModelValidator : AbstractValidator<CreateEventInputModel>
+    {
+        public CreateEventInputModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Nome não pode ser vazio/nulo");
+
+            RuleFor(x => x.Date)
+                .NotEmpty().WithMessage("Data não pode ser vazia/nula");
+                //.LessThan(DateTime.UtcNow.Date).WithMessage("A data não pode ser menor que hoje.");
+
+            RuleFor(x => x.Street)
+                .NotEmpty().WithMessage("Rua não pode ser vazia/nula");
+
+            RuleFor(x => x.Number)
+                .NotEmpty().WithMessage("Número não pode ser vazio/nulo");
+
+            RuleFor(x => x.City)
+                .NotEmpty().WithMessage("Cidade não pode ser vazia/nula");
+
+            RuleFor(x => x.State)
+                .NotEmpty().WithMessage("Estado não pode ser vazio/nulo");
+
+            RuleFor(x => x.ZipCode)
+                .NotEmpty().WithMessage("CEP não pode ser vazio/nulo");
+
+            RuleFor(x => x.Country)
+                .NotEmpty().WithMessage("País não pode ser vazio/nulo");
+        }
+    }
+}
---
+++ src/Application/UseCases/CreateEvent/CreateEventUseCase.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Domain.ValueObjects;
+using CrossCutting.Abstractions;
+
+namespace Application.Events.CreateEvent
+{
+    public class CreateEventUseCase : IUseCase<CreateEventInputModel, VoidResult>
+    {
+        private readonly IRepository<Event> _repository;
+        public CreateEventUseCase(IRepository<Event> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result<VoidResult>> Execute(CreateEventInputModel input)
+        {
+            var validationResult = new CreateEventInputModelValidator().Validate(input);
+
+            if (!validationResult.IsValid)
+            {
+                return Result<VoidResult>.Fail(validationResult.Errors.Select(e => e.ErrorMessage).ToList());
+            }
+
+            var address = new Address(input.Street, input.Number, input.City, input.State, input.ZipCode, input.Country, input.Complement);
+
+            var @event = new Event(input.Name, input.Description, input.Date, address);
+
+            await _repository.Add(@event);
+
+            await _repository.SaveChanges();
+
+            return Result<VoidResult>.Success(new VoidResult());
+        }
+    }
+}
